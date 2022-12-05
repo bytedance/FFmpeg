16c93947e6db9139dbad243fc1a0acd7c8e94256
baishuai <baishuai@bytedance.com>
2022-12-05 14:42:53 +0800

    avcodec/codec_id: add AV_CODEC_ID_MPEG2VIDEO_XVMC for old version compat

--- libavcodec/codec_id.h
+++ libavcodec/codec_id.h
@@ -49,6 +49,7 @@ enum AVCodecID {
     /* video codecs */
     AV_CODEC_ID_MPEG1VIDEO,
     AV_CODEC_ID_MPEG2VIDEO, ///< preferred ID for MPEG-1/2 video decoding
+    AV_CODEC_ID_MPEG2VIDEO_XVMC, // for compat with old version
     AV_CODEC_ID_H261,
     AV_CODEC_ID_H263,
     AV_CODEC_ID_RV10,
