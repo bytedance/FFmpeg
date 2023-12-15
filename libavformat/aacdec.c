4a8b887638af540b20483272f08d9d8f0fd978ab
liuxunfei <liuxunfei@bytedance.com>
2023-12-15 11:54:02 +0800

    feature: support get adts aac accurate duration

--- libavformat/aacdec.c
+++ libavformat/aacdec.c
@@ -22,6 +22,7 @@
 
 #include "libavutil/avassert.h"
 #include "libavutil/intreadwrite.h"
+#include "libavutil/opt.h"
 #include "avformat.h"
 #include "avio_internal.h"
 #include "internal.h"
@@ -30,6 +31,165 @@
 #include "apetag.h"
 
 #define ADTS_HEADER_SIZE 7
+
+typedef struct AACContext {
+    const AVClass *class;
+    int accurate_mode;
+} AACContext;
+
+static const AVOption options[] = {
+    { "aac_accurate_mode", "use accurate mode", offsetof(AACContext, accurate_mode), AV_OPT_TYPE_BOOL, {.i64 = 0}, 0, 1, AV_OPT_FLAG_AUDIO_PARAM},
+    { NULL },
+};
+
+static const AVClass ff_aac_demuxer_class = {
+    .class_name = "aac_dexmuxer",
+    .item_name  = av_default_item_name,
+    .option     = options,
+    .version    = LIBAVUTIL_VERSION_INT,
+    .category   = AV_CLASS_CATEGORY_DEMUXER,
+};
+
+static int adts_aac_resync(AVFormatContext *s)
+{
+    uint16_t state;
+
+    // skip data until an ADTS frame is found
+    state = avio_r8(s->pb);
+    while (!avio_feof(s->pb) && avio_tell(s->pb) < s->probesize) {
+        state = (state << 8) | avio_r8(s->pb);
+        if ((state >> 4) != 0xFFF)
+            continue;
+        avio_seek(s->pb, -2, SEEK_CUR);
+        break;
+    }
+    if (s->pb->eof_reached)
+        return AVERROR_EOF;
+    if ((state >> 4) != 0xFFF)
+        return AVERROR_INVALIDDATA;
+
+    return 0;
+}
+
+static int get_sample_rate(uint8_t index)
+{
+    static const int sample_rates[] = {
+        96000, 88200, 64000, 48000, 44100, 32000,
+        24000, 22050, 16000, 12000, 11025, 8000,
+        7350
+    };
+
+    if (index < sizeof(sample_rates) / sizeof(sample_rates[0])) {
+        return sample_rates[index];
+    }
+
+    return 0;
+}
+
+static int64_t get_adts_aac_frame_size(AVFormatContext *s, int64_t offset)
+{
+    const uint8_t kAdtsHeaderLengthNoCrc = 7;
+    const uint8_t kAdtsHeaderLengthWithCrc = 9;
+
+    uint8_t syncword[2];
+    avio_seek(s->pb, offset, SEEK_SET);
+    if(avio_read(s->pb, &syncword, 2) != 2) {
+        av_log(s, AV_LOG_ERROR, "failed to read adts syncword!\n");
+        return AVERROR_INVALIDDATA;
+    }
+    if ((syncword[0] != 0xff) || ((syncword[1] & 0xf6) != 0xf0)) {
+        av_log(s, AV_LOG_ERROR, "failed to read adts syncword, byte error!\n");
+        return AVERROR_INVALIDDATA;
+    }
+
+    uint8_t protection_absent;
+    avio_seek(s->pb, offset + 1, SEEK_SET);
+    if (avio_read(s->pb, &protection_absent, 1) < 1) {
+        av_log(s, AV_LOG_ERROR, "failed to read adts protection_absent!\n");
+        return AVERROR_INVALIDDATA;
+    }
+    protection_absent &= 0x1;
+
+    uint8_t header[3];
+    uint32_t frame_size;
+    avio_seek(s->pb, offset + 3, SEEK_SET);
+    if (avio_read(s->pb, &header, 3) < 3) {
+        av_log(s, AV_LOG_ERROR, "failed to read adts frame_size!\n");
+        return AVERROR_INVALIDDATA;
+    }
+    frame_size = (header[0] & 0x3) << 11 | header[1] << 3 | header[2] >> 5;
+
+    // protectionAbsent is 0 if there is CRC
+    uint8_t head_size = protection_absent ? kAdtsHeaderLengthNoCrc : kAdtsHeaderLengthWithCrc;
+    if (head_size > frame_size) {
+        av_log(s, AV_LOG_ERROR, "adts head_size greater than frame_size!\n");
+        return AVERROR_INVALIDDATA;
+    }
+
+    return frame_size;
+}
+
+static int adts_aac_accurate_read_header(AVFormatContext *s, AVStream *st, AVDictionary **options) {
+    int ret = 0;
+
+    int64_t stream_size = avio_size(s->pb);
+    if (stream_size <= 0) {
+        av_log(s, AV_LOG_ERROR, "avio_size error!\n");
+        return AVERROR_INVALIDDATA;
+    }
+
+    int64_t first_frame_pos = avio_tell(s->pb);
+    do {
+        uint8_t header[2];
+        avio_seek(s->pb, 2, SEEK_CUR);
+        if (avio_read(s->pb, &header, 2) < 2) {
+            av_log(s, AV_LOG_ERROR, "failed to read adts profile!\n");
+            ret = AVERROR_INVALIDDATA;
+            break;
+        }
+        int profile = (header[0] >> 6) & 0x3;
+        st->codecpar->profile = profile;
+
+        uint8_t index = (header[0] >> 2) & 0xf;
+        int sample_rate = get_sample_rate(index);
+        if (sample_rate == 0) {
+            av_log(s, AV_LOG_ERROR, "failed to read adts sample_rate!\n");
+            ret = AVERROR_INVALIDDATA;
+            break;
+        }
+        st->codecpar->sample_rate = sample_rate;
+
+        uint8_t channel = (header[0] & 0x1) << 2 | (header[1] >> 6);
+        if (channel == 0) {
+            av_log(s, AV_LOG_ERROR, "failed to read adts channels!\n");
+            ret = AVERROR_INVALIDDATA;
+            break;
+        }
+        st->codecpar->channels = channel;
+
+        avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
+
+        int64_t offset = 0;
+        int64_t frame_size = 0;
+        int64_t num_frames = 0;
+
+        while (offset < stream_size) {
+            if ((frame_size = get_adts_aac_frame_size(s, first_frame_pos + offset)) == 0) {
+                break;
+            }
+            offset += frame_size;
+            num_frames++;
+        }
+        // round up and get the duration
+        int64_t frame_duration_us = (1024 * 1000000ll + (sample_rate - 1)) / sample_rate;
+        int64_t duration_us = num_frames * frame_duration_us;
+        st->duration = av_rescale_q(duration_us, AV_TIME_BASE_Q, st->time_base);
+    } while (0);
+
+    avio_seek(s->pb, first_frame_pos, SEEK_SET);
+    
+    return ret;
+}
 
 static int adts_aac_probe(const AVProbeData *p)
 {
@@ -80,27 +240,6 @@ static int adts_aac_probe(const AVProbeData *p)
         return 0;
 }
 
-static int adts_aac_resync(AVFormatContext *s)
-{
-    uint16_t state;
-
-    // skip data until an ADTS frame is found
-    state = avio_r8(s->pb);
-    while (!avio_feof(s->pb) && avio_tell(s->pb) < s->probesize) {
-        state = (state << 8) | avio_r8(s->pb);
-        if ((state >> 4) != 0xFFF)
-            continue;
-        avio_seek(s->pb, -2, SEEK_CUR);
-        break;
-    }
-    if (s->pb->eof_reached)
-        return AVERROR_EOF;
-    if ((state >> 4) != 0xFFF)
-        return AVERROR_INVALIDDATA;
-
-    return 0;
-}
-
 static int adts_aac_read_header(AVFormatContext *s)
 {
     AVStream *st;
@@ -126,8 +265,16 @@ static int adts_aac_read_header(AVFormatContext *s)
     if (ret < 0)
         return ret;
 
-    // LCM of all possible ADTS sample rates
-    avpriv_set_pts_info(st, 64, 1, 28224000);
+    ret = -1;
+    AACContext *ctx = (AACContext *)s->priv_data;
+    if (ctx && ctx->accurate_mode == 1) {
+        ret = adts_aac_accurate_read_header(s, st, NULL);
+    }
+
+    if (ret < 0) {
+        // LCM of all possible ADTS sample rates
+        avpriv_set_pts_info(st, 64, 1, 28224000);
+    }
 
     return 0;
 }
@@ -216,4 +363,6 @@ AVInputFormat ff_aac_demuxer = {
     .extensions   = "aac",
     .mime_type    = "audio/aac,audio/aacp,audio/x-aac",
     .raw_codec_id = AV_CODEC_ID_AAC,
+    .priv_data_size = sizeof(AACContext),
+    .priv_class = &ff_aac_demuxer_class,
 };
